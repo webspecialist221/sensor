a0adafc3ac8ea3658ff438acccbc6b3dc2f04a27
Saddam Hussain <webspecialist221@users.noreply.github.com>
2017-11-06 20:18:29 +0800

    Upload All Data

---
+++ Final Project Code.ino
@@ -0,0 +1,94 @@
+#define trigPin 7
+#define echoPin 6
+#define led 4
+#define led2 5
+#define led3 8
+#define led4 9
+#define led5 10
+#define led6 11
+#define buzzer 3
+
+int sound = 250;
+
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+ 
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+ 
+
+  if (distance <= 30) {
+    digitalWrite(led, HIGH);
+    sound = 200;
+}
+  else {
+    digitalWrite(led,LOW);
+  }
+  if (distance < 25) {
+      digitalWrite(led2, HIGH);
+      sound = 250;
+}
+  else {
+      digitalWrite(led2, LOW);
+  }
+  if (distance < 20) {
+      digitalWrite(led3, HIGH);
+      sound = 300;
+} 
+  else {
+    digitalWrite(led3, LOW);
+  }
+  if (distance < 15) {
+    digitalWrite(led4, HIGH);
+    sound = 350;
+}
+  else {
+    digitalWrite(led4,LOW);
+  }
+  if (distance < 10) {
+    digitalWrite(led5, HIGH);
+    sound = 400;
+}
+  else {
+    digitalWrite(led5,LOW);
+  }
+  if (distance < 5) {
+    digitalWrite(led6, HIGH);
+    sound = 450;
+}
+  else {
+    digitalWrite(led6,LOW);
+  }
+ 
+  if (distance > 30 || distance <= 0){
+    Serial.println("Out of range");
+    noTone(buzzer);
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+    tone(buzzer, sound);
+   
+  }
+  delay(500);
+}
+
